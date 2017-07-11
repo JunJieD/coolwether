0bb7acd6d685e353795a9079d1890344b07fea88
JunJieD <530298015@qq.com>
2017-07-11 11:40:33 +0800

    初步完成。

--- app/src/main/java/com/example/dengjj900815/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/dengjj900815/coolweather/WeatherActivity.java
@@ -1,13 +1,18 @@
 package com.example.dengjj900815.coolweather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -17,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.dengjj900815.coolweather.gson.Forecast;
 import com.example.dengjj900815.coolweather.gson.Weather;
+import com.example.dengjj900815.coolweather.service.AutoUpdateService;
 import com.example.dengjj900815.coolweather.util.HttpUtil;
 import com.example.dengjj900815.coolweather.util.Utility;
 
@@ -27,7 +33,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends Activity {
-
+    public SwipeRefreshLayout swipeRefresh;//下拉刷新
+    private String mWeatherId;//当前城市天气id
     private ScrollView weatherLayout;//天气界面
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -40,15 +47,18 @@ public class WeatherActivity extends Activity {
     private TextView carWashText;
     private TextView sportText;
     private ImageView bingImg;//必应每日一图
+    public DrawerLayout drawerLayout;//侧滑界面
+    private Button sideSlip;//侧滑按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT >= 21) {//Android系统5.0以上
             View decorView = getWindow().getDecorView();
+            //背景图占满屏幕与系统状态栏融合
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);//设置状态栏透明
         }
         setContentView(R.layout.activity_weather);
         //初始化各控件
@@ -64,18 +74,30 @@ public class WeatherActivity extends Activity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
         bingImg = (ImageView) findViewById(R.id.bing_pic_img);
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);//设置刷新进度条颜色
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        sideSlip = (Button) findViewById(R.id.sideslip_button);
         SharedPreferences spf = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = spf.getString("weather", null);
         if (weatherString != null) {
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            mWeatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         } else {
             //无缓存时去服务器查询天气
+            mWeatherId = getIntent().getStringExtra("weather_id");
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
         }
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(mWeatherId);
+            }
+        });
         String bingPic = spf.getString("bing_pic", null);
         if (bingPic != null) {
             //有缓存时直接加载图片
@@ -84,12 +106,18 @@ public class WeatherActivity extends Activity {
             //无缓存时通过loadBingPic方法连接服务器加载
             loadBingPic();
         }
+        sideSlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);//打开滑动菜单
+            }
+        });
     }
 
     /**
      * 加载必应每日一图
      */
-    private void loadBingPic() {
+    public void loadBingPic() {
         String requestBingPic = "http://guolin.tech/api/bing_pic";
         HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
             @Override
@@ -117,10 +145,9 @@ public class WeatherActivity extends Activity {
 
     /**
      * 根据天气id请求城市天气信息
-     *
      * @param weatherId
      */
-    private void requestWeather(final String weatherId) {
+    public void requestWeather(final String weatherId) {
         //和风天气信息接口地址 CN101200206
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
                 weatherId + "&key=6cf92ca94def448cb70aacda50aaae1d";
@@ -132,6 +159,7 @@ public class WeatherActivity extends Activity {
                     @Override
                     public void run() {
                         Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                        swipeRefresh.setRefreshing(false);//刷新结束，隐藏进度条
                     }
                 });
             }
@@ -152,6 +180,7 @@ public class WeatherActivity extends Activity {
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefresh.setRefreshing(false);//刷新结束，隐藏进度条
                     }
                 });
             }
@@ -160,10 +189,9 @@ public class WeatherActivity extends Activity {
 
     /**
      * 处理并展示Weather 实体类中的数据
-     *
      * @param weather
      */
-    private void showWeatherInfo(Weather weather) {
+    public void showWeatherInfo(Weather weather) {
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
@@ -196,5 +224,7 @@ public class WeatherActivity extends Activity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 }
